732484a363e2ae6fe9c52c15501f001bc69b384a
Danil <d.sidyakin@tcore.team>
2024-09-10 21:23:36 +0300

    Add virtualization

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,19 +1,74 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { CSSProperties, useEffect, useRef, useState } from "react";
 import classes from "./Articles.module.scss";
 import { LoadedArticle } from "components/Articles/Loaded";
 import { Article } from "components/Articles/Article";
 import { useLoadArticles } from "components/Articles/useLoadArticles";
 
-const Loaders = () => Array.from({ length: 15 }).map(() => <LoadedArticle/>);
+const LOADER_COUNT = 15;
+const LOADER_SIZE = 530;
+const LOADERS_SIZE = LOADER_SIZE * LOADER_COUNT;
+
+const getStyle = (shift: number): CSSProperties => ({
+  transform: `translateY(${shift}px)`,
+});
+
+const Loaders = () =>
+  Array.from({ length: LOADER_COUNT }).map((_, index) => (
+    <LoadedArticle key={index} />
+  ));
 
 export const Articles = () => {
   const [page, setPage] = useState(0);
   const listEndRef = useRef<HTMLDivElement>(null);
   const listRef = useRef<HTMLDivElement>(null);
-  const articles = useLoadArticles({ listRef, page, listEndRef });
-  const nextPageObserver = useRef(new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) setPage(page => page + 1);
-  }));
+
+  const nextPageObserver = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((page) => page + 1);
+        }
+      },
+      { rootMargin: "-800px 0px 800px 0px" },
+    ),
+  );
+  const [cache, setCache] = useState<Array<number>>([0]);
+  const articles = useLoadArticles({ cache, page, listRef });
+  const [listLength, setListLength] = useState(LOADERS_SIZE);
+  const [scroll, setScroll] = useState(0);
+
+  const mutationObserver = useRef<ResizeObserver>(
+    new ResizeObserver((entries) => {
+      entries.forEach(({ target, borderBoxSize }) => {
+        const index = +target.getAttribute("data-index");
+        const size = borderBoxSize[0].blockSize;
+
+        if (size) {
+          setCache((cache) => {
+            const newCache = [...cache];
+            const next = index + 1;
+
+            if (newCache[next]) {
+              const delta = size + newCache[index] - newCache[next];
+
+              if (delta <= 0) return cache;
+              setListLength((listLength) => listLength + delta);
+
+              for (let j = next; j < newCache.length; j++) {
+                newCache[j] = newCache[j] + delta;
+              }
+            } else {
+              setListLength((listLength) => listLength + size);
+
+              if (!newCache[index]) newCache[index] = newCache[index - 1];
+              newCache[next] = index ? size + newCache[index] : size;
+            }
+            return newCache;
+          });
+        } else mutationObserver.current.unobserve(target);
+      });
+    }),
+  );
 
   useEffect(() => {
     const observer = nextPageObserver.current;
@@ -25,12 +80,44 @@ export const Articles = () => {
   }, []);
 
   return (
-    <div className={classes.list} ref={listRef}>
-      {articles.map((article) =>
-        <Article article={article} key={article.id}/>
-      )}
-      <div className={classes.loading} ref={listEndRef}>
-        <Loaders/>
+    <div
+      className={classes.list_container}
+      ref={listRef}
+      onScroll={({ currentTarget }) => {
+        setScroll(currentTarget.scrollTop);
+      }}
+    >
+      <div
+        style={{ height: listLength, position: "relative" }}
+        className={classes.list}
+      >
+        {articles.map((article, index) => {
+          const shift = cache[index];
+          if (
+            shift + 1000 < scroll ||
+            shift - 1000 > scroll + window.innerHeight
+          ) {
+            return null;
+          }
+
+          return (
+            <div
+              style={getStyle(shift)}
+              key={index}
+              data-index={index}
+              ref={(elem) => {
+                if (elem) {
+                  mutationObserver.current.observe(elem);
+                }
+              }}
+            >
+              <Article article={article} key={article.id}/>
+            </div>
+          );
+        })}
+        <div style={getStyle(cache.at(-1))} ref={listEndRef}>
+          <Loaders/>
+        </div>
       </div>
     </div>
   );
--- src/components/Articles/useLoadArticles.ts
+++ src/components/Articles/useLoadArticles.ts
@@ -2,27 +2,33 @@ import { MutableRefObject, useEffect, useState } from "react";
 import { articleApi } from "api/article";
 import { ArticleT } from "api/article/types";
 
+const COUNT = 15;
+
 type Props = {
   page: number;
+  cache: number[];
   listRef: MutableRefObject<HTMLDivElement>;
-  listEndRef: MutableRefObject<HTMLDivElement>;
-}
-export const useLoadArticles = ({page, listEndRef, listRef}: Props) => {
+};
+
+export const useLoadArticles = ({ page, cache, listRef }: Props) => {
   const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState<(ArticleT | null)[]>([]);
 
-
   useEffect(() => {
-    setLoading(true)
-    if (!loading) articleApi.all({ offset: page * 15, limit: 15 })
-      .then(({ data }) => {
-        setArticles((articles) => [...articles, ...data.results,]);
-        setLoading(false);
-        if (listRef.current.scrollTop > listEndRef.current.offsetTop) listRef.current.scrollTo(0, listEndRef.current.offsetTop - window.innerHeight);
-        console.log(listRef.current.scrollTop);
-      }).catch(()=>{});
+    setLoading(true);
+    console.log("=>setLoading");
+
+    if (!loading) {
+      articleApi
+        .all({ offset: page * COUNT, limit: COUNT })
+        .then(({ data }) => {
+          setArticles((articles) => [...articles, ...data.results]);
+          setLoading(false);
+        })
+        .catch(() => {});
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
   return articles;
-}
+};
